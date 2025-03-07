5fd6a72271aef70606071bf5ab98bd51ab260ff6
Shahd Ali <shahdali102004@gmail.com>
2025-03-07 22:55:11 +0200

    Updated code and responsive

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ function colors(color){
     container.style.background =color;
 
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navMenu = document.querySelector("ul");
+
+    menuIcon.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+        // change the style of the arrow
+        if (navMenu.classList.contains("active")) {
+            menuIcon.textContent = "▲"; 
+        } else {
+            menuIcon.textContent = "▼"; 
+        }
+    });
+});
